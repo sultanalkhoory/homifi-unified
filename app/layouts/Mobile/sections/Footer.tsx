316c895f9729332b67ea011b12fa9fb6e8e1f650
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-10-05 21:16:06 +0400

    Delete app directory

--- app/layouts/Mobile/sections/Footer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Footer() {
-  return (
-    <footer className="py-16 text-center bg-white">
-      <p className="text-sm text-gray-500">Designed in Dubai. Built for your home.</p>
-    </footer>
-  );
-}
