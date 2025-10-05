88de92a44d6f7779d0eb18ed452414e46eadad12
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-10-05 21:48:00 +0400

    Update SecuritySection.tsx

--- app/layouts/Mobile/sections/SecuritySection.tsx
+++ app/layouts/Mobile/sections/SecuritySection.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion, useInView } from "framer-motion";
+import Image from "next/image";
 import IPhoneFrame from "@/components/ui/IPhoneFrame";
 
 export default function SecuritySection() {
